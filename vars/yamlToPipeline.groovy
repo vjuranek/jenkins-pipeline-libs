1a68d878bd25e8d667eda42ca34282b776fa8531
Vojtech Juranek <vojtech.juranek@gmail.com>
2018-06-06 14:22:25 +0200

    test

--- vars/yamlToPipeline.groovy
+++ vars/yamlToPipeline.groovy
@@ -2,14 +2,20 @@
 import org.yaml.snakeyaml.Yaml
 
 def loadStages(String yamlPath) {
+  def pipe
+  
   node {
     stage('Checkout') {
       checkout([$class: 'GitSCM', branches: [[name: '*/pipelines']], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'RelativeTargetDirectory', relativeTargetDir: 'jdg-qe']], submoduleCfg: [], userRemoteConfigs: [[credentialsId: '20d2c03a-3a3a-4dcf-be31-956071aac2c3', url: 'git@github.com:infinispan/jdg-qe.git']]])
     }
+    
+    stage('Load-pipeline') {
+        Yaml yaml = new Yaml()
+	pipe = yaml.load((yamlPath as File).text)
+    }
   }
-  
-  Yaml yaml = new Yaml()
-  yaml.load((yamlPath as File).text)
+
+  pipe
 }
 
 def jobParams(Map params) {
