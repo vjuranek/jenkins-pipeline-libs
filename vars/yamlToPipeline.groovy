2a433778e05eaf297ddfd53ca2ab7d55e9aee99d
Vojtech Juranek <vojtech.juranek@gmail.com>
2018-06-07 10:13:03 +0200

    test

--- vars/yamlToPipeline.groovy
+++ vars/yamlToPipeline.groovy
@@ -4,20 +4,16 @@ import org.yaml.snakeyaml.Yaml
 def loadStages(String yamlPath) {
   def pipe
   
-  node {
-    // stage('Checkout') {
-    //   checkout([$class: 'GitSCM', branches: [[name: '*/pipelines']], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'RelativeTargetDirectory', relativeTargetDir: 'jdg-qe']], submoduleCfg: [], userRemoteConfigs: [[credentialsId: '20d2c03a-3a3a-4dcf-be31-956071aac2c3', url: 'git@github.com:infinispan/jdg-qe.git']]])
-    // }
+  //  node {
+    stage('Checkout') {
+      checkout([$class: 'GitSCM', branches: [[name: '*/pipelines']], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'RelativeTargetDirectory', relativeTargetDir: 'jdg-qe']], submoduleCfg: [], userRemoteConfigs: [[credentialsId: '20d2c03a-3a3a-4dcf-be31-956071aac2c3', url: 'git@github.com:infinispan/jdg-qe.git']]])
+    }
     
     stage('Load-pipeline') {
-      withCredentials([sshUserPrivateKey(credentialsId: '20d2c03a-3a3a-4dcf-be31-956071aac2c3')]) {
-	sh "git clone git@github.com:infinispan/jdg-qe.git"
-	sh "ls -la > /tmp/ls"
-      }
       Yaml yaml = new Yaml()
       pipe = yaml.load(("/home/jenkins/workspace/jdg-pipeline-full/" + yamlPath as File).text)
     }
-  }
+    //  }
 
   pipe
 }
